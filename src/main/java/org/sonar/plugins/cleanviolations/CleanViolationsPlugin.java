41695091817c1d2b3ccc3310c4522fb2538f25ba
Pablo D. Roca <pablodroca@gmail.com>
2013-02-22 13:45:32 -0300

    New widget implementation already working.

--- src/main/java/org/sonar/plugins/cleanviolations/CleanViolationsPlugin.java
+++ src/main/java/org/sonar/plugins/cleanviolations/CleanViolationsPlugin.java
@@ -7,16 +7,36 @@ import org.sonar.api.Property;
 import org.sonar.api.PropertyField;
 import org.sonar.api.PropertyType;
 import org.sonar.api.SonarPlugin;
+import org.sonar.plugins.cleanviolations.CleanViolationsWidget;
 
 import java.util.List;
 
+
+@Properties({
+  @Property(
+    key = "PROPERTY1",
+    name = "Property 1 Name",
+    description = "Property 1 is used to define the plugin behavior",
+    project = true,
+    global = true,
+    fields = {
+      @PropertyField(
+        key = "PROPERTY2",
+        name = "Property 2",
+        description = "Property 2 description.",
+        type = PropertyType.STRING,
+        indicativeSize = CleanViolationsPlugin.LARGE_SIZE)})
+})
 public final class CleanViolationsPlugin extends SonarPlugin {
 
   static final int LARGE_SIZE = 20;
   static final int SMALL_SIZE = 10;
 
-  public List<Class<? extends BatchExtension>> getExtensions() {
-    return null;
+  @SuppressWarnings("unchecked")
+  public List getExtensions() {
+    return ImmutableList.of(
+		CleanViolationsWidget.class
+		);
   }
 
 }
